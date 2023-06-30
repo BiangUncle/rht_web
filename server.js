a8f3006f7d84514865e7b7a9bd422d04ceba792b
biang <595974382@qq.com>
2023-06-30 10:34:39 +0800

    项目终止

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
 app.use(bodyParser.json()); // Parses json, multi-part (file), url-encoded
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type， Accept");
-    next();
-})
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type， Accept");
+//     next();
+// })
 app.use('/public', express.static('public'));
 app.use('/pages', express.static('pages'));
 app.use('/sdk', express.static('sdk'));
